db8218eeff695bc2d1788c542bdbd67c5f61f7a1
Loïc SORROCHE <loic.sorroche@isen.yncrea.fr>
2023-01-17 16:33:39 +0100

    passage au points d'avancement 3 e

--- app/src/main/java/fr/isen/sorroche/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/sorroche/androiderestaurant/CategoryActivity.kt
@@ -1,14 +1,17 @@
 package fr.isen.sorroche.androiderestaurant
 
 
-
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 import fr.isen.sorroche.androiderestaurant.databinding.ActivityCategoryBinding
+import org.json.JSONObject
 
 
 class CategoryActivity : AppCompatActivity() {
@@ -18,18 +21,30 @@ class CategoryActivity : AppCompatActivity() {
         binding = ActivityCategoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val title = binding.titre
         title.text = intent.getStringExtra("title")
 
         val dishes = resources.getStringArray(R.array.plats).toList() as ArrayList<String>
         binding.recyclerviewListMenu.layoutManager = LinearLayoutManager(this)
-        binding.recyclerviewListMenu.adapter = CustomAdapter(dishes){
-            val intent = Intent(this,DetailActivity::class.java)
+        binding.recyclerviewListMenu.adapter = CustomAdapter(arrayListOf()) {
+            val intent = Intent(this, DetailActivity::class.java)
             startActivity(intent)
         }
-
+        loadDishesFromAPI()
     }
-
-
+    private fun loadDishesFromAPI(){
+        Volley.newRequestQueue(this)
+        val url="http://test.api.catering.bluecodegames.com/menu"
+        val jsonObject = JSONObject()
+        jsonObject.put("id_shop","1")
+        val jsonRequest = JsonObjectRequest(Request.Method.POST, url, jsonObject,
+            {
+            Log.w("categoryActivity","response : $it")
+        }, {
+            Log.w("categoryActivity","erreur : $it")
+            })
+        Volley.newRequestQueue(this).add(jsonRequest)
+    }
 
 }
--- app/src/main/java/fr/isen/sorroche/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/sorroche/androiderestaurant/DetailActivity.kt
@@ -2,6 +2,15 @@ package fr.isen.sorroche.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
