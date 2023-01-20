d296837611aef89948cfd63de3e94b91751bb27a
Loïc SORROCHE <loic.sorroche@isen.yncrea.fr>
2023-01-20 15:12:21 +0100

    passage au points d'avancement 4

--- app/src/main/java/fr/isen/sorroche/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/sorroche/androiderestaurant/CategoryActivity.kt
@@ -1,9 +1,9 @@
 package fr.isen.sorroche.androiderestaurant
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
@@ -11,10 +11,9 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
-
 import fr.isen.sorroche.androiderestaurant.databinding.ActivityCategoryBinding
-import fr.isen.sorroche.androidrestaurant.model.Data
 import fr.isen.sorroche.androidrestaurant.model.DataResult
+import fr.isen.sorroche.androidrestaurant.model.Items
 import org.json.JSONObject
 
 
@@ -29,7 +28,6 @@ class CategoryActivity : AppCompatActivity() {
         val title = binding.titre
         title.text = intent.getStringExtra("title")
 
-        val dishes = resources.getStringArray(R.array.plats).toList() as ArrayList<String>
         binding.recyclerviewListMenu.layoutManager = LinearLayoutManager(this)
         binding.recyclerviewListMenu.adapter = CustomAdapter(arrayListOf()) {
             val intent = Intent(this, DetailActivity::class.java)
@@ -57,6 +55,7 @@ class CategoryActivity : AppCompatActivity() {
         val dishCategory=dishesResult.data.firstOrNull{it.nameFr == intent.getStringExtra("title") }
 
         val adapter = binding.recyclerviewListMenu.adapter as CustomAdapter
-        adapter.refreshList(dishCategory?.items?.map {it.nameFr} as ArrayList<String>)
+        adapter.refreshList(dishCategory?.items as ArrayList<Items>)
     }
+
 }
--- app/src/main/java/fr/isen/sorroche/androiderestaurant/CustomAdapter.kt
+++ app/src/main/java/fr/isen/sorroche/androiderestaurant/CustomAdapter.kt
@@ -1,20 +1,41 @@
 package fr.isen.sorroche.androiderestaurant
 
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import fr.isen.sorroche.androidrestaurant.model.Items
 
 
-class CustomAdapter(var dataSet: ArrayList<String>, val onItemClickListener: () -> Unit) :
+class CustomAdapter(var dataSet: ArrayList<Items>, val onItemClickListener: () -> Unit) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView
-        init {
-            // Define click listener for the ViewHolder's View
-            textView = view.findViewById(R.id.textView)
+        val textView: TextView = view.findViewById(R.id.textView)
+        val image :ImageView = view.findViewById(R.id.imageview)
+        val prices :TableLayout = view.findViewById(R.id.prices)
+         fun setPrices(elem: Items) {
+            elem.prices.forEach { price ->
+                val row = TableRow(prices.context)
+
+                val size = TextView(prices?.context)
+                size.textSize = 12f
+                row.addView(size)
+
+                val prix = TextView(prices?.context)
+                prix.gravity = Gravity.END
+                prix.textSize = 12f
+                prix.text = price.price.toString()+ "€"
+                row.addView(prix)
+
+                prices?.addView(row)
+            }
         }
     }
 
@@ -32,7 +53,14 @@ class CustomAdapter(var dataSet: ArrayList<String>, val onItemClickListener: () 
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.textView.text = dataSet[position]
+        viewHolder.textView.text = dataSet[position].nameFr
+        viewHolder.setPrices(dataSet[position])
+
+        val firstImage = dataSet[position].images[0]
+        if (firstImage.isNotEmpty()) {
+            Picasso.get().load(firstImage).resize(50, 50).into(viewHolder.image)
+        }
+
         viewHolder.itemView.setOnClickListener{
             onItemClickListener()
         }
@@ -41,9 +69,10 @@ class CustomAdapter(var dataSet: ArrayList<String>, val onItemClickListener: () 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
 
-    fun refreshList(dishesFromAPI: ArrayList<String>){
+    fun refreshList(dishesFromAPI: ArrayList<Items>){
         dataSet =dishesFromAPI
         notifyDataSetChanged()
     }
+
 
 }
