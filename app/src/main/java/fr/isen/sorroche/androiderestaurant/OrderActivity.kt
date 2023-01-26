b843e5787f43c0474a148d83569ce8cd4ab66c4d
Loïc SORROCHE <loic.sorroche@isen.yncrea.fr>
2023-01-26 17:32:04 +0100

    ecrire plusieur plats dans le fichier bouton + - magret cassé???

--- app/src/main/java/fr/isen/sorroche/androiderestaurant/OrderActivity.kt
+++ app/src/main/java/fr/isen/sorroche/androiderestaurant/OrderActivity.kt
@@ -1,11 +1,18 @@
 package fr.isen.sorroche.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 import fr.isen.sorroche.androiderestaurant.databinding.ActivityDetailBinding
 import fr.isen.sorroche.androiderestaurant.databinding.ActivityOrderBinding
+import fr.isen.sorroche.androiderestaurant.model.Panier
+import fr.isen.sorroche.androiderestaurant.model.Plats
+import fr.isen.sorroche.androidrestaurant.model.Items
 import java.io.File
+import java.io.FileInputStream
 
 class OrderActivity : AppCompatActivity() {
 
@@ -18,7 +25,22 @@ class OrderActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.payOrder.setOnClickListener {passOrder()}
+
+        val file = File(cacheDir.absolutePath,"panier_courant.json")
+        if (file.exists()){
+            val inputStream = FileInputStream(file)
+            val buffer = ByteArray(inputStream.available())
+            inputStream.read(buffer)
+            val jsonString = String(buffer)
+            val gson=Gson()
+            val tabPanierEnregistre = gson.fromJson(jsonString,Panier::class.java)
+            val pEnregistre =tabPanierEnregistre.panierObjectList
+
+            binding.recyclerViewPanier.layoutManager = LinearLayoutManager(this)
+            binding.recyclerViewPanier.adapter = OrderAdapter(pEnregistre)
+        }
     }
+
 
     fun passOrder(){
         var file = File(cacheDir.absolutePath,"panier_courant.json")
--- app/src/main/java/fr/isen/sorroche/androiderestaurant/OrderAdapter.kt
+++ app/src/main/java/fr/isen/sorroche/androiderestaurant/OrderAdapter.kt
@@ -1,25 +1,47 @@
 package fr.isen.sorroche.androiderestaurant
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import fr.isen.sorroche.androiderestaurant.model.Plats
 import fr.isen.sorroche.androidrestaurant.model.Items
 
-class OrderAdapter (var dataSet: ArrayList<Items>, val onItemClickListener: (Items) -> Unit) :
+class OrderAdapter (var dataSet: List<Plats>) :
     RecyclerView.Adapter<OrderAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.name_plat_order)
+        val quality: TextView = view.findViewById(R.id.quantity_plat_order)
+        val price: TextView = view.findViewById(R.id.tot_price_plat_order)
 
+        fun setPrices(plats: Plats){
+            //todo
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderAdapter.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.order_row_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: OrderAdapter.ViewHolder, position: Int) {
+
+        viewHolder.name.text = dataSet[position].plat.nameFr
+        //viewHolder.setPrices(dataSet[position])
+        viewHolder.quality.text=dataSet[position].nb_plat.toString()
     }
 
     override fun getItemCount()= dataSet.size
 
-    override fun onBindViewHolder(holder: OrderAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    fun refreshList(contenu_panier: List<Plats>){
+        dataSet = contenu_panier
+        notifyDataSetChanged()
     }
 
 }
